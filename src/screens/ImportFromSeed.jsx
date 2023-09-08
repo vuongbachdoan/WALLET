2b6bbe0d79cf1a2fc5c3e1fe18354eee00a33520
vuongbachdoan <vuongbachdoan@gmail.com>
2023-09-09 00:16:41 +0700

    fix: on blur

--- src/screens/ImportFromSeed.jsx
+++ src/screens/ImportFromSeed.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { Input, Icon, Button } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 
@@ -7,45 +7,51 @@ export const ImportFromSeed = ({ navigation }) => {
     const [showPassword, setShowPassword] = React.useState(false);
 
     return (
-        <View style={styles.container}>
-            <View style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                columnGap: 12,
-                width: '100%'
-            }}>
-                <Input
-                    placeholder="Seed Phrase"
-                    flex={1}
-                    height='64px'
-                    fontSize={14}
-                    type={showPassword ? 'text' : 'password'}
-                    borderColor='#181E25'
-                    color='#FFF'
-                    borderWidth={1}
-                    borderRadius={15}
-                    InputRightElement={
-                        <Icon
-                            as={<MaterialIcons name={showPassword ? "visibility" : "visibility-off"} />}
-                            size="xl"
-                            mr={2}
-                            onPress={() => setShowPassword(!showPassword)}
-                        />
-                    }
-                />
-                <Button
-                    size="sm"
-                    width='64px'
-                    height='64px'
-                    borderColor='#181E25'
-                    borderWidth={1}
-                    borderRadius={15}
-                    backgroundColor='#202832'
-                    rightIcon={<Icon as={<MaterialIcons name="qr-code-scanner" />} size="xl" />}
-                >
-                </Button>
-            </View>
+        <View
+            style={styles.container}
+        >
+            <ScrollView
+                keyboardShouldPersistTaps='handled'
+            >
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    columnGap: 12,
+                    width: '100%'
+                }}>
+                    <Input
+                        placeholder="Seed Phrase"
+                        flex={1}
+                        height='64px'
+                        fontSize={14}
+                        type={showPassword ? 'text' : 'password'}
+                        borderColor='#181E25'
+                        color='#FFF'
+                        borderWidth={1}
+                        borderRadius={15}
+                        InputRightElement={
+                            <Icon
+                                as={<MaterialIcons name={showPassword ? "visibility" : "visibility-off"} />}
+                                size="xl"
+                                mr={2}
+                                onPress={() => setShowPassword(!showPassword)}
+                            />
+                        }
+                    />
+                    <Button
+                        size="sm"
+                        width='64px'
+                        height='64px'
+                        borderColor='#181E25'
+                        borderWidth={1}
+                        borderRadius={15}
+                        backgroundColor='#202832'
+                        rightIcon={<Icon as={<MaterialIcons name="qr-code-scanner" />} size="xl" />}
+                    >
+                    </Button>
+                </View>
+            </ScrollView>
         </View>
     );
 };
